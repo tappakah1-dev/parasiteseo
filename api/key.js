09358efb7acb905a962293c7e5b61ceb4a715d58
tappakah1-dev <tappakah1@gmail.com>
2026-03-14 12:51:26 +0000

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,5 +1,7 @@
-export default function handler(req, res) {
+export default function handler(req,res){
+
 res.status(200).json({
-key: process.env.GEMINI_API_KEY
+key:process.env.GEMINI_API_KEY
 });
+
 }
